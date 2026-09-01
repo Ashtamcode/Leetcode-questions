1cb1202e78c442afdd42abda86bc44f78ac84295
Ashtamcode <70945937+Ashtamcode@users.noreply.github.com>
2026-09-01 18:31:09 +0530

    Time: 0 ms (100%), Space: 17.7 MB (95.04%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        int V = numCourses;
+        vector<int>adj[V];
+        for(int i=0;i<prerequisites.size();i++){
+            int u = prerequisites[i][0];
+            int v = prerequisites[i][1];
+            adj[v].push_back(u);
+        }
+        vector<int>indegree(V,0);
+        for(int i=0;i<V;i++){
+            for(auto it:adj[i]){
+                indegree[it]++;
+            }
+        }
+        queue<int>q;
+        for(int i=0;i<V;i++){
+            if(indegree[i] == 0) q.push(i);
+        }
+        vector<int>topo;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+            for(auto it:adj[node]){
+                indegree[it]--;
+                if(indegree[it] == 0) q.push(it);
+                
+            }
+        }
+        if(topo.size() !=V) return {};
+        return topo;
+    }
+};
